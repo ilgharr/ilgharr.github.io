c68853c9697a68393a8346cfab1ae032f263e322
ilghar rahno <69784418+ilgharr@users.noreply.github.com>
2022-08-18 13:45:11 -0400

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,7 +1,8 @@
 
 function hiringRadio(){
     if(document.getElementById('hiring').checked){
-        document.getElementById('hiring_input').innerHTML = "<label for='salary_rate'>Salary: </label> <input class='form_input' type='text' id='salary_rate' name='salary_rate' value='Enter dollars and cents 99.99' pattern='\d\d' required>"
+        let ptrn = /\d*\.\d{1,2}$/i 
+        document.getElementById('hiring_input').innerHTML = "<label for='salary_rate'>Salary: </label> <input class='form_input' type='text' id='salary_rate' name='salary_rate' value='Enter dollars and cents 99.99' pattern=" + ptrn + " required>"
     }
     else{
         document.getElementById('hiring_input').innerHTML = ""
